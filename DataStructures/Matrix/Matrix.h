e64d78f6edbb7ff3a407ceb1f94ed8d8606926b1
kinfi4 <ilyamakarov223@gmail.com>
2021-04-09 23:24:51 +0300

    get determinant function created

--- DataStructures/Matrix/Matrix.h
+++ DataStructures/Matrix/Matrix.h
@@ -6,6 +6,8 @@
 #define MATRIX_MATRIX_H
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -40,7 +42,12 @@ public:
 
     }
     double get_determinant(){
+        double res = 0;
+        for (int i = 0; i < this->ndim; ++i) {
+            res += pow(-1, 0 + i) * this->matrix[0][i] * this->_get_det_for_minor(vector<int>{0}, vector<int>{i});
+        }
 
+        return res;
     }
     vector<double>& operator [] (int index){
         return this->matrix[index];
@@ -110,8 +117,41 @@ private:
     int ndim;
     vector<vector<double>> matrix;
 
-    double _get_det_for_minor(){
+    double _get_det_for_minor(vector<int> deleted_rows, vector<int> deleted_cols){
+        double res = 0;
 
+        if(deleted_cols.size() == this->ndim - 1)
+            return this->_find_intersected_element(deleted_rows, deleted_cols);
+
+        int cur_row = 0;
+        for (int i = 0; i < this->ndim; ++i)
+            if(not count(deleted_rows.begin(), deleted_rows.end(), i)){
+                cur_row = i;
+                break;
+            }
+
+        deleted_rows.push_back(cur_row);
+
+        int count_column_index = 0;
+        for (int i = 0; i < this->ndim; ++i) {
+            if(not count(deleted_cols.begin(), deleted_cols.end(), i)){
+                auto new_deleted_columns = deleted_cols;
+                new_deleted_columns.push_back(i);
+                res += pow(-1, 0 + count_column_index) * this->matrix[cur_row][i] * this->_get_det_for_minor(deleted_rows, new_deleted_columns);
+                count_column_index++;
+            }
+        }
+
+        return res;
+    }
+    double _find_intersected_element(vector<int>& deleted_rows, vector<int>& deleted_cols){
+        for (int i = 0; i < this->ndim; ++i) {
+            if(not count(deleted_rows.begin(), deleted_rows.end(), i))
+                for (int j = 0; j < this->ndim; ++j) {
+                    if(not count(deleted_cols.begin(), deleted_cols.end(), j))
+                        return this->matrix[i][j];
+                }
+        }
     }
 
 };
--- DataStructures/Matrix/main.cpp
+++ DataStructures/Matrix/main.cpp
@@ -4,13 +4,17 @@
 using namespace std;
 
 int main() {
-    auto matrix = SquareMatrix(3, 10);
+    auto matrix = SquareMatrix(4, 1);
 
     matrix[0][0] = 12;
+    matrix[1][1] = 12;
+    matrix[2][1] = 35;
+    matrix[3][3] = 55;
     matrix++;
     matrix++;
     matrix--;
     matrix.print_matrix();
+    cout << matrix.get_determinant();
 //    cout << matrix[0][0] << endl;
 
     return 0;
